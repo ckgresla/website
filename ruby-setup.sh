b6279b2b9d14e7a8da868bedfe5a12dfc82d587b
Chris Kerwell Gresla <chris@lindy.ai>
2023-08-12 15:11:14 -0700

    initalized the website after a tad bit of hacking

---
+++ ruby-setup.sh
@@ -0,0 +1,3 @@
+echo "source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh" >> ~/.zshrc
+echo "source $(brew --prefix)/opt/chruby/share/chruby/auto.sh" >> ~/.zshrc
+echo "chruby ruby-3.1.3" >> ~/.zshrc # run 'chruby' to see actual version
